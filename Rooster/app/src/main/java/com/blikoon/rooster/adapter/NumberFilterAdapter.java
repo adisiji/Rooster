a382a008e410b730c6890ca74c09158affc90bf1
Adi Dar <madidarma1@gmail.com>
2016-10-31 20:26:25 +0700

    Added submodule MemorizingTrustManager

---
+++ Rooster/app/src/main/java/com/blikoon/rooster/adapter/NumberFilterAdapter.java
@@ -0,0 +1,85 @@
+package com.blikoon.rooster.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.blikoon.rooster.R;
+import com.blikoon.rooster.model.Contact;
+
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by neobyte on 10/15/2016.
+ */
+
+public class NumberFilterAdapter extends RecyclerView.Adapter<NumberFilterAdapter.ContactHolder>
+{
+    private List<Contact> mContacts;
+    private mClickListener klik;
+
+    public interface mClickListener {
+        public void mClick(Contact contact);
+    }
+
+    public NumberFilterAdapter(List<Contact> contactList, mClickListener klik)
+    {
+        this.klik = klik;
+        mContacts = contactList;
+    }
+
+    @Override
+    public ContactHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater
+                .inflate(R.layout.list_item_contact, parent,
+                        false);
+        return new ContactHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ContactHolder holder, int position) {
+        Contact contact = mContacts.get(position);
+        holder.bindContact(contact, klik);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContacts.size();
+    }
+
+    public class ContactHolder extends RecyclerView.ViewHolder
+    {
+        private TextView contactTextView;
+        private Contact mContact;
+        public ContactHolder (final View itemView)
+        {
+            super(itemView);
+            contactTextView = (TextView) itemView.findViewById(R.id.contact_jid);
+        }
+
+
+        public void bindContact(final Contact contact, final mClickListener mKlik)
+        {
+            mContact = contact;
+            if (mContact == null)
+            {
+                Log.d(TAG,"Trying to work on a null Contact object ,returning.");
+                return;
+            }
+            contactTextView.setText(mContact.getJid());
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mKlik.mClick(contact);
+                }
+            });
+        }
+    }
+}
